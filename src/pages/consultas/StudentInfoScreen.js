815d475110062e6368c5d323252f35f7aff50bb7
Ruben <skyline7514@gmail.com>
2021-12-08 20:08:06 -0700

    propuesta de diseño de tabla

--- src/pages/consultas/StudentInfoScreen.js
+++ src/pages/consultas/StudentInfoScreen.js
@@ -1,118 +1,47 @@
 import React from 'react'
-import { Table } from 'react-bootstrap';
 import { Layout } from '../../components/layout/Layout'
-import { image } from "../../helpers/image";
 
 export const StudentInfoScreen = () => {
     return (
         <Layout>
-            <div className="titulo">
-                <h2>Datos generales</h2>
-                <hr/>
-            </div>
-            <div className="principal">
-                <div>
-                    <img
-                    className="fotoPerfil"
-                    src={ image('./UAS.png').default }
-                    alt="foto de perfil"
-                    ></img>
-                </div>
-                <div className="tablas">
-                    <h5>DATOS DEL ALUMNO</h5>
-                    <Table striped bordered hover>
+                <div className="container">
+                <table>
                         <thead>
                             <tr>
-                            <th>#</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Username</th>
+                                <th>No. Cuenta</th>
+                                <th>Nombre</th>
+                                <th>Sexo</th>
+                                <th>Fecha Nacimiento</th>
+                                <th>Fecha Ingreso</th>
+                                <th>Calle</th>
+                                <th>Colonia</th>
+                                <th>Codigo Postal</th>
+                                <th>Poblacion</th>
+                                <th>Localidad</th>
+                                <th>Telefono</th>
+                                <th>CURP</th>
+                                <th>Afiliacion IMSS</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                            <td>1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            </tr>
-                            <tr>
-                            <td>2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                            </tr>
-                            <tr>
-                            <td>3</td>
-                            <td colSpan="2">Larry the Bird</td>
-                            <td>@twitter</td>
+                                <td>1232323232323</td>
+                                <td>adrian figueroa</td>
+                                <td> M </td>
+                                <td>31/05/1997</td>
+                                <td>31/05/1997</td>
+                                <td>AVENIDA</td>
+                                <td>PRADOS</td>
+                                <td>SINALOA</td>
+                                <td>CULIACAN, ROSALES</td>
+                                <td>66719315153</td>
+                                <td>121231312313</td>
+                                <td>C1D2DSWED12</td>
+                                <td>ASADFW11212</td>
                             </tr>
                         </tbody>
-                    </Table>
-                </div>
-                <div className="tablas">
-                    <h5>DATOS DEL PADRE O TUTOR</h5>
-                    <table>
-                        <tr>
-                            <th>Padre</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>No. Padre</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>Calle</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>Colonia</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>Codigo Postal</th>
-                            <th><input type="text"></input></th>
-                        </tr><tr>
-                            <th>Poblacion</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>Telefono</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                    </table>
-                    <br/>
-                    <h5>ESTUDIOS</h5>
-                    <table>
-                        <tr>
-                            <th>Escuela</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>Carrera</th>
-                            <th><input type="text"></input></th>
-                        </tr><tr>
-                            <th>Plan de estudios</th>
-                            <th><input type="text"></input></th>
-                        </tr>
-                        <tr>
-                            <th>Periodo</th>
-                            <th><input type="text"></input></th>
-                        </tr><tr>
-                            <th>Grupo</th>
-                            <th><input type="text"></input></th>
-                        </tr><tr>
-                            <th>Vigencia</th>
-                            <th><input type="text"></input></th>
-                        </tr>
                     </table>
                 </div>
-                {/*<div className="tablas">
-                    <h5>ESTUDIOS</h5>
-                   
-                </div>
-                */}
-            </div>
         </Layout>
     )
 }
