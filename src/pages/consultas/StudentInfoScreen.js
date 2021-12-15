c97f570db636d2319b24d3bf2ffaf628ee81714d
Ruben <skyline7514@gmail.com>
2021-12-15 16:07:36 -0700

    datos generales

--- src/pages/consultas/StudentInfoScreen.js
+++ src/pages/consultas/StudentInfoScreen.js
@@ -5,13 +5,21 @@ import {Table} from '../../components/UI/Table';
 
 
 let list=require('../../assets/images/studentInfo.json')
-const colums=Object.keys(list[0])
-const title="DATOS DEL ALUMNOS"
+let list2=require('../../assets/images/padre.json')
+let list3=require('../../assets/images/carrera.json')
+let colums=Object.keys(list[0])
+let colums2=Object.keys(list2[0])
+let colums3=Object.keys(list3[0])
+let title="DATOS DEL ALUMNOS"
+let title2="DATOS DEL PADRE"
+let title3="CARRERA"
 
 export const StudentInfoScreen = () => {
     return (
         <Layout>
           <Table list={list} colums={colums} title={title}/>
+          <Table list={list2} colums={colums2} title={title2}/>
+          <Table list={list3} colums={colums3} title={title3}/>
         </Layout>
     )
 }
