1695bd9558e59ac3832ed7de60a76631b80a1fc3
Ruben <skyline7514@gmail.com>
2021-12-24 21:39:08 -0700

    desmadre

--- src/pages/consultas/KardexScreen.js
+++ src/pages/consultas/KardexScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Layout } from '../../components/layout/Layout';
+import { PDFViewer } from '@react-pdf/renderer';
+import MyDocument from '../../components/UI/Pdf';
 
 
 
 export const KardexScreen = () => {
     return (
         <Layout>
-            <div>
-               <h1>Kardex</h1>
-            </div>
+            <PDFViewer style={{width:"100%",height:"90vh"}}>
+                <MyDocument/>
+            </PDFViewer>
         </Layout>
     );
 }
