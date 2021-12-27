594e0c69716fbd1d67861c0056487f52f40d1e5d
Osiris Meza <oamh09@gmail.com>
2021-12-27 14:56:44 -0700

    validator añadido | cambios moderados en el navbar

--- src/components/UI/Navbar/NavButtonProfile.jsx
+++ src/components/UI/Navbar/NavButtonProfile.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
+import { useUser } from '../../../hooks/useUser';
 
 export const NavButtonProfile = (props) => {
+
+    const { user } = useUser();
+    const name = user.nombre.split(" ")[0];
+
     const { icon, state, ...others } = props;
     useEffect(() => {
         const isProfile = document.querySelector('.dropdown .btn-nav-uas[profile]');
@@ -14,9 +19,14 @@ export const NavButtonProfile = (props) => {
     }, [state])
 
     return (
-        <button className="nav-link | btn-nav-uas" { ...others }>
-            <i className={(icon) ? icon : ""}></i>
-            <span>{ props.children }</span>
+        <button className="nav-link | btn-nav-uas | d-flex | align-items-center" { ...others }>
+            <img className="nav-profile--picture | me-2 | ms-2 | rounded-circle" 
+                alt="Profile"
+                width={32}
+                height={32}
+                src={user.foto}
+            />
+            <span>{ name }</span>
             <i className="ps-2 | d-lg-inline-block | d-none | fas fa-chevron-down | "></i>
         </button>
     )
---
+++ src/components/UI/Navbar/NavbarBrand.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import { image } from '../../../helpers/image';
+
+export const NavbarBrand = () => {
+    return (
+        <Link className="navbar-brand | p-1" to="/">
+            <img
+                src={image('./UAS.png').default}
+                alt="UAS logo"
+                width="25"
+                height="30"
+                className="d-inline-block | align-text-top" />{' '}
+            <span className="title | d-lg-inline-block | d-none">Universidad Autonoma de Sinaloa</span>
+            <span className="title | d-inline-block | d-lg-none">UAS</span>
+        </Link>
+    )
+}
