ea1676f07e9d09a340285aa0cc07cae0842c1031
Osiris Meza <oamh09@gmail.com>
2022-01-07 11:15:59 -0700

    deploy

--- src/components/UI/Navbar/NavbarContent.jsx
+++ src/components/UI/Navbar/NavbarContent.jsx
@@ -1,17 +1,5 @@
-// import { NavProfile } from '../Profile/NavProfile';
 import { NavItem } from './NavItem';
-
 import * as routes from '../../../routers/routes.json';
-
-const { 
-    consultas,
-    tramites,
-    reinscripcion,
-    examenes,
-    idiomas,
-    encuestas
-} = routes;
-
 export const NavbarContent = ({isNavOpen}) => {
     return (
         <div
@@ -23,12 +11,12 @@ export const NavbarContent = ({isNavOpen}) => {
                 <hr className='uas-divider' />
             </div>
             <ul className="navbar-nav | mb-lg-0">
-                <NavItem route={ consultas }     icon="fas fa-search"/>
-                <NavItem route={ reinscripcion } icon="fas fa-user-graduate"/>
-                <NavItem route={ tramites }      icon="fas fa-file-invoice"/>
-                <NavItem route={ examenes }      icon="far fa-edit"/>
-                <NavItem route={ idiomas }       icon="fas fa-globe"/>
-                <NavItem route={ encuestas }     icon="fas fa-poll"/>
+                <NavItem route={ routes.consultas }     icon="fas fa-search"/>
+                <NavItem route={ routes.reinscripcion } icon="fas fa-user-graduate"/>
+                <NavItem route={ routes.tramites }      icon="fas fa-file-invoice"/>
+                <NavItem route={ routes.examenes }      icon="far fa-edit"/>
+                <NavItem route={ routes.idiomas }       icon="fas fa-globe"/>
+                <NavItem route={ routes.encuestas }     icon="fas fa-poll"/>
             </ul>
         </div>
     )
